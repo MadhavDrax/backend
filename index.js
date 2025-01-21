f8be1a7dd45c1afad60312b2406e01e12372e2d3
MadhavDrax <93091299+MadhavDrax@users.noreply.github.com>
2025-01-21 15:33:17 +0530

    fix url in cros

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ connectDB();
 
 // Middleware
 const corsOptions = {
-  origin: 'https://your-frontend-domain.com', // Replace with your frontend URL
+  origin: 'https://healthbuddy-x0enff.flutterflow.app', // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 };
